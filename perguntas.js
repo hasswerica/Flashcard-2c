8071d0df77e5c2a477f443ecf8af8c423d71de92
hasswerica <141144963+hasswerica@users.noreply.github.com>
2025-05-12 14:46:07 +0000

    OK

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Programação',
+    'O que é JavaScrip',
+    'Linguagem de programação',
+)
+
+criaCartao(
+    'Inglês',
+    'Como se escreve pagamento',
+    'Pay day',
+)
+
+criaCartao(
+    'Matemática',
+    'Qual o gráfico de função quarática',
+    'Parábola',
+)
