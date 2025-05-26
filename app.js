b65ab4434a335ee918d3e37f6db94f3fe664e0ba
hasswerica <141144963+hasswerica@users.noreply.github.com>
2025-05-26 14:58:07 +0000

    OK

--- app.js
+++ app.js
@@ -1,17 +1,5 @@
-criaCartao(
-    'Programação',
-    'O que é JavaScrip',
-    'Linguagem de programação',
-)
-
-criaCartao(
-    'Inglês',
-    'Como se escreve pagamento',
-    'Pay day',
-)
-
-criaCartao(
-    'Matemática',
-    'Qual o gráfico de função quarática',
-    'Parábola',
-)
+function criaCartao (catgoria, pergunta, resposta){
+    let container = document.getElementById('container')
+    let cartao = document.createElement('article')
+    cartao.className = 'cartao'
+}
