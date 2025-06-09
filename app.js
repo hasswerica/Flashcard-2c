12558bfe6f52285ee9e50017189dea1a8651e7c3
hasswerica <141144963+hasswerica@users.noreply.github.com>
2025-06-09 14:30:24 +0000

    OK

--- app.js
+++ app.js
@@ -2,4 +2,23 @@ function criaCartao (catgoria, pergunta, resposta){
     let container = document.getElementById('container')
     let cartao = document.createElement('article')
     cartao.className = 'cartao'
+    cartao.innerHTML =  `
+    <article class="cartao">
+                <div class="cartao_conteudo">
+                    <h3>${categoria}</h3>
+                    <div class="cartao_conteudo_pergunta" div>
+                        <p>${pergunta}<p>
+                    </div>
+                    <div class="cartao_conteudo_resposta">
+                       <p>${resposta}<p>
+                   </div>
+                </div>
+            </article>
+            let respostaEstaVisivel = false
+            function viraCartao (){
+            respotaEstaVisivel =!respostaEstaVisivel
+            cartao.classlist.toogle('article', respostaEtaVisivel)
+            }
+            cartao.addEventLIsten
+    `
 }
